743b35a2669fc683e33ec0bddbc8ce65855e4e6b
guggulla-pradeepreddy <pradeep.gv230493@gmail.com>
2015-12-04 19:02:47 +0530

    initial commit

---
+++ mvcpro/Controllers/ClockdisplayController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcpro.Controllers
+{
+    public class ClockdisplayController : Controller
+    {
+        //
+        // GET: /Clockdisplay/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
